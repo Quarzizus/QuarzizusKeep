ac78b9f0e81d83f4073686c03e9015286e43ca6d
Miguel Vásquez <angelzizu06@gmail.com>
2021-12-26 13:53:02 -0500

    :recycle: HOC DroppableContainer

---
+++ src/components/containerTasks/DroppableProvider.tsx
@@ -0,0 +1,54 @@
+import {
+  closestCenter,
+  DndContext,
+  DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { SetStateAction } from "react";
+
+interface props {
+  children: JSX.Element;
+  items: any[];
+  setItems: React.Dispatch<React.SetStateAction<any[]>>;
+}
+
+const DroppableProvider = ({ children, items, setItems }: props) => {
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+  const handleDragEnd = (e: any) => {
+    const { active, over } = e;
+    if (active.id !== over.id) {
+      setItems((items: any[]) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  };
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <SortableContext items={items} strategy={verticalListSortingStrategy}>
+        {children}
+      </SortableContext>
+    </DndContext>
+  );
+};
+
+export { DroppableProvider };
--- src/components/containerTasks/index.tsx
+++ src/components/containerTasks/index.tsx
@@ -2,22 +2,8 @@ import { TaskCard } from "../taskCard";
 import { Task } from "../task";
 import "./styles.css";
 import Masonry from "react-masonry-css";
-
-import {
-  closestCenter,
-  DndContext,
-  KeyboardSensor,
-  PointerSensor,
-  useSensor,
-  useSensors,
-} from "@dnd-kit/core";
-import {
-  arrayMove,
-  SortableContext,
-  sortableKeyboardCoordinates,
-  verticalListSortingStrategy,
-} from "@dnd-kit/sortable";
 import { useState } from "react";
+import { DroppableProvider } from "./DroppableProvider";
 
 const ContainerTasks = (): JSX.Element => {
   const [items, setItems] = useState([
@@ -34,24 +20,6 @@ const ContainerTasks = (): JSX.Element => {
     "10",
     "11",
   ]);
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    })
-  );
-
-  function handleDragEnd(e: any) {
-    const { active, over } = e;
-    if (active.id !== over.id) {
-      setItems((items) => {
-        const oldIndex = items.indexOf(active.id);
-        const newIndex = items.indexOf(over.id);
-
-        return arrayMove(items, oldIndex, newIndex);
-      });
-    }
-  }
 
   const breakpointColumnsObj = {
     default: 4,
@@ -61,25 +29,19 @@ const ContainerTasks = (): JSX.Element => {
   };
 
   return (
-    <DndContext
-      sensors={sensors}
-      collisionDetection={closestCenter}
-      onDragEnd={handleDragEnd}
-    >
-      <SortableContext items={items} strategy={verticalListSortingStrategy}>
-        <Masonry className="Masonry" breakpointCols={breakpointColumnsObj}>
-          {items.map((numerito: string): JSX.Element => {
-            return (
-              <TaskCard
-                key={numerito}
-                id={numerito}
-                title={`Title ${numerito}`}
-              />
-            );
-          })}
-        </Masonry>
-      </SortableContext>
-    </DndContext>
+    <DroppableProvider items={items} setItems={setItems}>
+      <Masonry className="Masonry" breakpointCols={breakpointColumnsObj}>
+        {items.map((numerito: string): JSX.Element => {
+          return (
+            <TaskCard
+              key={numerito}
+              id={numerito}
+              title={`Title ${numerito}`}
+            />
+          );
+        })}
+      </Masonry>
+    </DroppableProvider>
   );
 };
 
